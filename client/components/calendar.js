9b826d250acf1922ce167aee88c2848ec3e6734a
Christina Armstrong <armstrongch6@gmail.com>
2019-04-26 20:19:40 -0400

    installed and added the calendar component

---
+++ client/components/calendar.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+import Calendar from 'react-calendar'
+
+class Sample extends Component {
+  state = {
+    value: new Date()
+  }
+  onChange = value => this.setState({value})
+  render() {
+    const {value} = this.state
+
+    return (
+      <div className="Sample">
+        <header>
+          <h1>react-calendar sample page</h1>
+        </header>
+        <div className="Sample__container">
+          <main className="Sample__container__content">
+            <Calendar onChange={this.onChange} showWeekNumbers value={value} />
+          </main>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Sample
